14731c051ca5ed2e2d915365ef64edad48562796
Eduardo González <egonzalezburns@gmail.com>
2018-09-22 11:49:40 -0300

    First Steps

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
 });
-server.listen(1337, function() { });
+server.listen(6969, function() { });
 
 // create the server
 wsServer = new WebSocketServer({
@@ -17,12 +17,14 @@ wsServer = new WebSocketServer({
 // WebSocket server
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
+  console.log('Se conectó un Wonejo');
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
+      console.log("Ha llegado Carta!");
     if (message.type === 'utf8') {
-      // process WebSocket message
+      console.log(message);
     }
   });
 
